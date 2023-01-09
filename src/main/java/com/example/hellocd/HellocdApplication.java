539d9ff4f31fa2cce5cfe645b2e97c327743f582
lagerloa <acke_lagerlof@hotmail.com>
2023-01-09 09:08:22 +0000

    updated hello world message

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuous World!";
 	}
 
 	public static void main(String[] args) {
